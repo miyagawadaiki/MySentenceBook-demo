3677750b8c114c3e6dfce50a6a115a754c8abf87
miyagawadaiki <miyagawa.daiki.18@shizuoka.ac.jp>
2019-07-04 21:48:25 +0900

    don't work, this is checkpoint

--- sentences/urls.py
+++ sentences/urls.py
@@ -5,14 +5,22 @@ from . import views
 
 app_name = 'sentences'
 urlpatterns = [
+    # main
     path('', views.IndexView.as_view(), name='index'),
     path('create/', views.CreateView.as_view(), name='create'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+    # category
     path('category/', views.CategoryIndexView.as_view(), name='cate_index'),
     path('category/create/', views.CategoryCreateView.as_view(), name='cate_create'),
     path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='cate_detail'),
     path('category/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='cate_update'),
     path('category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='cate_delete'),
+    # tags
+    path('tag/', views.TagIndexView.as_view(), name='tag_index'),
+    path('tag/create/', views.TagCreateView.as_view(), name='tag_create'),
+    path('tag/<int:pk>/', views.TagDetailView.as_view(), name='tag_detail'),
+    path('tag/<int:pk>/update/', views.TagUpdateView.as_view(), name='tag_update'),
+    path('tag/<int:pk>/delete/', views.TagDeleteView.as_view(), name='tag_delete'),
 ]
--- sentences/views.py
+++ sentences/views.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views import generic
 
-from .models import Category, Sentence
+from .models import Category, Sentence, Tag
 
+
+##############################################
+# main views
+##############################################
 
 class IndexView(generic.ListView):
     template_name = 'sentences/index.html'
@@ -15,7 +20,8 @@ class IndexView(generic.ListView):
     context_object_name = 'sentence_list'
 
     def get_queryset(self):
-        return Sentence.objects.all().order_by('-updated_date')
+        return Sentence.objects.filter(author=self.request.user).order_by('-updated_date')[:50]
+        #return Sentence.objects.all().order_by('-updated_date')
 
 
 class DetailView(generic.DetailView):
@@ -33,7 +39,7 @@ class DetailView(generic.DetailView):
 class CreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Sentence
     template_name = 'sentences/form.html'
-    fields = ['sentence_text', 'comment_text', 'category']  # '__all__'
+    fields = ['sentence_text', 'comment_text', 'category', 'tag']  # '__all__'
     #context_object_name = 'sentence'
 
     def form_valid(self, form):
@@ -47,7 +53,7 @@ class CreateView(LoginRequiredMixin, generic.edit.CreateView):
 class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):  # The LoginRequired mixin
     model = Sentence
     template_name = 'sentences/form.html'
-    fields = ['sentence_text', 'comment_text', 'category']  # '__all__'
+    fields = ['sentence_text', 'comment_text', 'category', 'tag']  # '__all__'
     #context_object_name = 'sentence'
 
     def dispatch(self, request, *args, **kwargs):
@@ -74,13 +80,22 @@ class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):  # The LoginRequi
         return super(DeleteView, self).dispatch(request, *args, **kwargs)
 
 
+
+##############################################
+# category views
+##############################################
+
 class CategoryIndexView(generic.ListView):
     template_name = 'categories/index.html'
     paginate_by = 5
     context_object_name = 'category_list'
 
     def get_queryset(self):
-        return Category.objects.all().order_by('-created_date')
+        return Category.objects.filter(
+            Q(is_public=True) |
+            Q(author=self.request.user)
+        ).distinct().order_by('-created_date')[:50]
+        #return Category.objects.all().order_by('-created_date')
 
 
 class CategoryDetailView(generic.DetailView):
@@ -133,3 +148,66 @@ class CategoryDeleteView(LoginRequiredMixin, generic.edit.DeleteView):  # The Lo
         return super(CategoryDeleteView, self).dispatch(request, *args, **kwargs)
 
 
+
+##############################################
+# category views
+##############################################
+
+class TagIndexView(generic.ListView):
+    template_name = 'tags/index.html'
+    paginate_by = 5
+    context_object_name = 'tag_list'
+
+    def get_queryset(self):
+        return Tag.objects.filter(author=self.request.user).order_by('-used_date')[:50]
+        #return Category.objects.all().order_by('-created_date')
+
+
+class TagDetailView(generic.DetailView):
+    model = Tag
+    template_name = 'tags/detail.html'
+    context_object_name = 'tag'
+
+
+class TagCreateView(LoginRequiredMixin, generic.edit.CreateView):
+    model = Tag
+    template_name = 'tags/form.html'
+    fields = ['name']  # '__all__'
+    #context_object_name = 'sentence'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        # https://docs.djangoproject.com/en/2.0/topics/class-based-views/generic-editing/#models-and-request-user
+        form.instance.author = self.request.user
+        return super(TagCreateView, self).form_valid(form)
+
+
+class TagUpdateView(LoginRequiredMixin, generic.edit.UpdateView):  # The LoginRequired mixin
+    model = Tag
+    template_name = 'tags/form.html'
+    fields = ['name']  # '__all__'
+    #context_object_name = 'sentence'
+
+    def dispatch(self, request, *args, **kwargs):
+        # ownership validation
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to edit.')
+
+        return super(TagUpdateView, self).dispatch(request, *args, **kwargs)
+
+
+class TagDeleteView(LoginRequiredMixin, generic.edit.DeleteView):  # The LoginRequired mixin
+    model = Tag
+    success_url = reverse_lazy('sentences:tag_index')
+    template_name = 'tags/confirm_delete.html'
+    context_object_name = 'tag'
+
+    def dispatch(self, request, *args, **kwargs):
+        # ownership validation
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to delete.')
+
+        return super(TagDeleteView, self).dispatch(request, *args, **kwargs)
